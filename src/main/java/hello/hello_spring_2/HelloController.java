0c0002e1fd528c0bd6758d124946bbe34577cf7d
honginSon <sonhi2000@naver.com>
2024-06-14 16:11:01 +0900

    thymeleat 템플릿 엔진

---
+++ src/main/java/hello/hello_spring_2/HelloController.java
@@ -0,0 +1,17 @@
+package hello.hello_spring_2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
