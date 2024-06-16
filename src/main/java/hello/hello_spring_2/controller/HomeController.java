8eb4a81d956c58057501426bd0297f1e0eb0db1b
honginSon <sonhi2000@naver.com>
2024-06-16 22:57:10 +0900

    회원 웹 기능 - 조회

--- src/main/java/hello/hello_spring_2/controller/HomeController.java
+++ src/main/java/hello/hello_spring_2/controller/HomeController.java
@@ -4,8 +4,11 @@ import hello.hello_spring_2.domain.Member;
 import hello.hello_spring_2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -35,5 +38,12 @@ public class HomeController {
         memberService.save(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
