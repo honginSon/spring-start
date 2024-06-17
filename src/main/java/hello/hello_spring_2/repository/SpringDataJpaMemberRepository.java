ec8cc4414d1c0dc4e6e94c69f825fef3e8810066
honginSon <sonhi2000@naver.com>
2024-06-17 15:50:29 +0900

    스프링 데이터 JPA를 사용한 리포지토리 구현

---
+++ src/main/java/hello/hello_spring_2/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.hello_spring_2.repository;
+
+import hello.hello_spring_2.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
